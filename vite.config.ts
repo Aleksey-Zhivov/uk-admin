697e25c783bd0ea46ee961dc353be1b17c228faf
Алексей Живов <zhivovaa@baltech.ru>
2025-09-25 11:05:20 +0300

    env corrected

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-const repo = process.env.VITE_GH_REPO;
-const base = repo ? `/${repo}/` : "/";
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const repo = env.VITE_GH_REPO || "";
+  const base = repo ? `/${repo}/` : "/";
 
-export default defineConfig({
-  base: base,
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
+  return {
+    base,
+    plugins: [react()],
+    resolve: { alias: { "@": path.resolve(__dirname, "src") } },
+  };
 });
