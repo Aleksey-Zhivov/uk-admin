096abc858aff7245eb734441819a05169bf2a53a
Алексей Живов <zhivovaa@baltech.ru>
2025-09-25 10:42:02 +0300

    github deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const repo = process.env.VITE_GH_REPO;
+const base = repo ? `/${repo}/` : "/";
+
 export default defineConfig({
+  base: base,
   plugins: [react()],
   resolve: {
     alias: {
